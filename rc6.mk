993ed665dd481e50c31b38997d7361714804f243
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-04-13 03:32:37 +0000

    + rc6.mk

---
+++ rc6.mk
@@ -0,0 +1,14 @@
+# Makefile for RC6
+ALGO_NAME := RC6
+
+# comment out the following line for removement of RC6 from the build process
+BLOCK_CIPHERS += $(ALGO_NAME)
+
+
+$(ALGO_NAME)_OBJ      := rc6.o
+$(ALGO_NAME)_TEST_BIN := main-rc6-test.o debug.o uart.o serial-tools.o \
+                         rc6.o nessie_bc_test.o \
+                         nessie_common.o cli.o performance_test.o
+$(ALGO_NAME)_NESSIE_TEST      := test nessie
+$(ALGO_NAME)_PEROFRMANCE_TEST := performance
+
