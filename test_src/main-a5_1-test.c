7c4486d3328c45462fd276916cb94b80b48725fc
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-09-21 21:38:10 +0000

    forgotten file, my fault

---
+++ test_src/main-a5_1-test.c
@@ -0,0 +1,86 @@
+/* main-a5_1-test.c */
+/*
+    This file is part of the Crypto-avr-lib/microcrypt-lib.
+    Copyright (C) 2008  Daniel Otte (daniel.otte@rub.de)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+ * A5/1 test-suit
+ * 
+*/
+
+#include "config.h"
+#include "serial-tools.h"
+#include "uart.h"
+#include "debug.h"
+
+#include <A5_1.h>
+#include "nessie_stream_test.h"
+
+#include <stdint.h>
+#include <string.h>
+
+char* cipher_name = "A5_1";
+
+/*****************************************************************************
+ *  additional validation-functions											 *
+ *****************************************************************************/
+void a51_genctx_dummy(uint8_t* key, uint16_t keysize, void* ctx){
+	a5_1_init(ctx,key, keysize, NULL, 0);
+}
+
+
+
+void testrun_nessie_a51(void){
+	nessie_stream_ctx.outsize_b =   8; /* actually unused */
+	nessie_stream_ctx.keysize_b =  64; 
+	nessie_stream_ctx.ivsize_b =   64;
+	nessie_stream_ctx.name = cipher_name;
+	nessie_stream_ctx.ctx_size_B = sizeof(a5_1_ctx_t);
+	nessie_stream_ctx.cipher_genctx = (nessie_stream_genctx_fpt)a51_genctx_dummy;
+	nessie_stream_ctx.cipher_enc = (nessie_stream_genenc_fpt)a5_1_gen;
+	
+	nessie_stream_run();	
+}
+
+
+
+/*****************************************************************************
+ *  main																	 *
+ *****************************************************************************/
+
+int main (void){
+	char  str[20];
+	DEBUG_INIT();
+	uart_putstr("\r\n");
+
+	uart_putstr_P(PSTR("\r\n\r\nCrypto-VS ("));
+	uart_putstr(cipher_name);
+	uart_putstr_P(PSTR(")\r\nloaded and running\r\n"));
+
+restart:
+	while(1){ 
+		if (!getnextwordn(str,20))  {DEBUG_S("DBG: W1\r\n"); goto error;}
+		if (strcmp(str, "nessie")) {DEBUG_S("DBG: 1b\r\n"); goto error;}
+			testrun_nessie_a51();
+		goto restart;		
+		continue;
+	error:
+		uart_putstr("ERROR\r\n");
+	}
+	
+	
+}
+
