a1518b457d04b4d5819e6ce4d7815eab53f937da
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2008-12-11 18:14:49 +0000

    seed now in pure asm

--- mkfiles/seed.mk
+++ mkfiles/seed.mk
@@ -4,7 +4,7 @@ ALGO_NAME := SEED
 # comment out the following line for removement of SEED from the build process
 BLOCK_CIPHERS += $(ALGO_NAME)
 
-$(ALGO_NAME)_OBJ      := seed-stub.o seed-asm.o
+$(ALGO_NAME)_OBJ      := seed-asm.o
 $(ALGO_NAME)_TEST_BIN := main-seed-test.o debug.o uart.o serial-tools.o \
                          nessie_bc_test.o nessie_common.o \
                          cli.o performance_test.o
