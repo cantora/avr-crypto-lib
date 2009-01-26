6918fd7223cd8f27b8438169e6aed8e4110a562a
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-01-26 18:04:30 +0000

    serpent sboxes in asm bitslice

--- mkfiles/serpent_asm_bitslice.mk
+++ mkfiles/serpent_asm_bitslice.mk
@@ -1,11 +1,11 @@
 # Makefile for serpent
-ALGO_NAME := SERPENT_BITSLICE
+ALGO_NAME := SERPENT_ASM_BITSLICE
 
 # comment out the following line for removement of serpent from the build process
 BLOCK_CIPHERS += $(ALGO_NAME)
 
 
-$(ALGO_NAME)_OBJ      := serpent-asm.o serpent-sboxes-bitslice-asm.o memxor.o
+$(ALGO_NAME)_OBJ      := serpent-sboxes-bitslice-asm.o serpent-asm.o memxor.o
 $(ALGO_NAME)_TEST_BIN := main-serpent-test.o debug.o uart.o serial-tools.o \
                          nessie_bc_test.o nessie_common.o cli.o performance_test.o
 $(ALGO_NAME)_NESSIE_TEST      := "nessie"
