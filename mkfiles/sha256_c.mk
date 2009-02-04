aa477226f97401661d134365ebd65080f23c2d12
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-02-04 13:58:25 +0000

    updating to the new configuration

--- mkfiles/sha256_c.mk
+++ mkfiles/sha256_c.mk
@@ -6,7 +6,8 @@ HASHES += $(ALGO_NAME)
 
 $(ALGO_NAME)_OBJ      := sha256.o
 $(ALGO_NAME)_TEST_BIN := main-sha256-test.o debug.o uart.o serial-tools.o \
-                         nessie_hash_test.o nessie_common.o cli.o string-extras.o performance_test.o
+                         nessie_hash_test.o nessie_common.o cli.o string-extras.o performance_test.o \
+			 hfal-basic.o hfal_sha256.o shavs.o dump.o
 $(ALGO_NAME)_NESSIE_TEST      := "nessie"
 $(ALGO_NAME)_PERFORMANCE_TEST := "performance"
 
