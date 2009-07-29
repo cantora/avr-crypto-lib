cccb1d3e008592ad7e4b57653afb95deb4d4a2c5
bg <bg@b1d182e4-1ff8-0310-901f-bddb46175740>
2009-07-29 10:34:15 +0000

    added md5_c.mk

---
+++ mkfiles/md5_c.mk
@@ -0,0 +1,11 @@
+# Makefile for MD5
+ALGO_NAME := MD5_C
+
+# comment out the following line for removement of MD5 from the build process
+HASHES += $(ALGO_NAME)
+
+$(ALGO_NAME)_OBJ      := md5.o
+$(ALGO_NAME)_TEST_BIN := main-md5-test.o hfal_md5.o $(CLI_STD) $(HFAL_STD)
+$(ALGO_NAME)_NESSIE_TEST      := "nessie"
+$(ALGO_NAME)_PERFORMANCE_TEST := "performance"
+
