ddc66bc0543c50a0f36a3c26abf90dfef36dc3d9
bg <daniel.otte@rub.de>
2012-12-26 21:41:51 +0100

    [keccak-asm] removing rotate64 from asm build

--- mkfiles/keccak_asm.mk
+++ mkfiles/keccak_asm.mk
@@ -6,7 +6,7 @@ HASHES += $(ALGO_NAME)
 
 $(ALGO_NAME)_DIR      := keccak/
 $(ALGO_NAME)_INCDIR   := memxor/ hfal/
-$(ALGO_NAME)_OBJ      := keccak-stub.o keccak-asm.o memxor.o rotate64.o
+$(ALGO_NAME)_OBJ      := keccak-stub.o keccak-asm.o memxor.o 
 $(ALGO_NAME)_TESTBIN  := main-keccak-test.o hfal_keccak.o  $(CLI_STD) $(HFAL_STD)
 $(ALGO_NAME)_NESSIE_TEST      := test nessie
 $(ALGO_NAME)_PERFORMANCE_TEST := performance
