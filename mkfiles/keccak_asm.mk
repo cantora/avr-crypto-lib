690a1cfe4c9b27d2b6c4b217022ddc6d20445c5e
bg <daniel.otte@rub.de>
2013-01-20 23:00:17 +0100

    [keccak-asm] final changes

--- mkfiles/keccak_asm.mk
+++ mkfiles/keccak_asm.mk
@@ -6,7 +6,7 @@ HASHES += $(ALGO_NAME)
 
 $(ALGO_NAME)_DIR      := keccak/
 $(ALGO_NAME)_INCDIR   := memxor/ hfal/
-$(ALGO_NAME)_OBJ      := keccak-stub.o keccak-asm.o memxor.o 
+$(ALGO_NAME)_OBJ      := keccak-asm.o
 $(ALGO_NAME)_TESTBIN  := main-keccak-test.o hfal_keccak.o  $(CLI_STD) $(HFAL_STD)
 $(ALGO_NAME)_NESSIE_TEST      := test nessie
 $(ALGO_NAME)_PERFORMANCE_TEST := performance
